a463c00b2fc725729f201f51059b8dcde2546b17
Vlasios Santorinios <80896196+Santor8@users.noreply.github.com>
2024-08-17 14:31:33 +0300

    Add files via upload

---
+++ js_ics20122.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function addTheValueOly(secondValue) {
+    var fValue = document.getElementById("ValueOly");
+    ValueOly.innerHTML = parseInt(fValue.innerHTML) +
+    parseInt(secondValue);
+ }
+ 
+ function addTheValuePana(secondValue) {
+    var fValue = document.getElementById("ValuePana");
+    ValuePana.innerHTML = parseInt(fValue.innerHTML) +
+    parseInt(secondValue);
+ }
+ 
